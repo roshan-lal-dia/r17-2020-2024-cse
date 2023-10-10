7db151a9cba2bf8ee7d2a54c068e6e7c9a332dbe
Roshan Lal Dia <91119120+roshan-lal-dia@users.noreply.github.com>
2023-10-10 05:58:02 +0000

    fixed

--- s7-8761/CeaserCipher.java
+++ s7-8761/CeaserCipher.java
@@ -1,7 +1,5 @@
 public class CeaserCipher{
- String name = "Test";
-}
-
-public static void main(String args[]) {
-    System.out.println("hello World");
+    public static void main(String[] args) {
+        System.out.println("hello World");
+    }
 }
