aa4b99970def6090939453b0db34cac715ae0e4c
Roshan Lal Dia <91119120+roshan-lal-dia@users.noreply.github.com>
2023-11-20 22:12:22 +0530

    Update CaesarCipher.java --added code from GPT-3.5

--- s7-8761/CaesarCipher.java
+++ s7-8761/CaesarCipher.java
@@ -1,11 +1,33 @@
 //ex.1.a) Caesar Cipher
-public class CaesarCipher{
+public class CaesarCipher {
+    public static String encrypt(String text, int shift) {
+        StringBuilder encryptedText = new StringBuilder();
+        for (char character : text.toCharArray()) {
+            if (Character.isAlphabetic(character)) {
+                char shiftedChar = (char) (((Character.toUpperCase(character) + shift - 'A') % 26 + 'A'));
+                encryptedText.append(Character.isLowerCase(character) ? Character.toLowerCase(shiftedChar) : shiftedChar);
+            } else {
+                encryptedText.append(character);
+            }
+        }
+        return encryptedText.toString();
+    }
 
-
-
+    public static String decrypt(String encryptedText, int shift) {
+        return encrypt(encryptedText, -shift);
+    }
 
     public static void main(String[] args) {
-        System.out.println("hello World");
+        String plaintext = "Hello, World!";
+        int shift = 3;
+
+        String encryptedText = encrypt(plaintext, shift);
+        String decryptedText = decrypt(encryptedText, shift);
+
+        System.out.println("Original Text: " + plaintext);
+        System.out.println("Encrypted Text: " + encryptedText);
+        System.out.println("Decrypted Text: " + decryptedText);
     }
 }
+
 
